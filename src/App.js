42bcfc49f09d98c106c307ffc85654b131a963b7
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-11-24 09:07:14 +0200

    Initialized repository

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { AuthProvider } from "./components/AuthContext";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Header />
+        <div className="container py-3">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            {/* <Route path="/hotels" element={<Hotels />} /> */}
+            {/* <Route path="/hotel/:id" element={<Hotel />} /> */}
+            {/* <Route path="/hotel/create" element={<Hotel />} /> */}
+            {/* <Route path="/countries" element={<Countries />} /> */}
+            {/* <Route path="/countries/:id" element={<Country />} /> */}
+            {/* <Route path="/countries/create" element={<Country />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* <Route path="/orders" element={<Orders />} /> */}
+          </Routes>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
